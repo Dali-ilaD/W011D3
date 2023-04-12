fba8a9bbc96b3f8230a565dc931cab9214d22941
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-04-12 13:48:50 -0700

    Climate and lightswitch

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,24 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+import { createContext, useContext, useState } from 'react';
+export const ClimateContext = createContext();
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider({ children }) {
+  const [climateTemp, setClimateTemp] = useState("50");
+  const [climateHumid, setClimateHumid] = useState("40%");
+
+  return (
+    <ClimateContext.Provider
+      value={{
+        climateTemp,
+        setClimateTemp,
+        climateHumid,
+        setClimateHumid
+      }}
+    >
+      {children}
+    </ClimateContext.Provider>
+  );
+}
+
